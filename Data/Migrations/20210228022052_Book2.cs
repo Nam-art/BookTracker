95fa97bd2f4e37cd567607827c8f2c0f43445509
unknown <sagetbob349@gmail.com>
2021-02-27 21:51:00 -0600

    Initial commit.

---
+++ Data/Migrations/20210228022052_Book2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookTracker.Data.Migrations
+{
+    public partial class Book2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
